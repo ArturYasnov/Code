dd29f70e68034cfa6274516bac6596613c5b48b4
ArturYasnov <65731060+ArturYasnov@users.noreply.github.com>
2021-07-21 21:16:16 +0300

    Update main.py

--- Detections/src/main.py
+++ Detections/src/main.py
@@ -1,4 +1,91 @@
-..
+# ====================================================
+# Library
+# ====================================================
+import requests
+import shutil
+from pathlib import Path
+from contextlib import contextmanager
+from collections import defaultdict, Counter
+
+import scipy as sp
+import numpy as np
+import pandas as pd
+
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold
+
+from tqdm.auto import tqdm
+from functools import partial
+
+from PIL import Image
+
+import cv2
+from skimage import io
+import torch
+from torch import nn
+import os
+from datetime import datetime
+import time
+import random
+import torchvision
+from torchvision import transforms
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import timm
+from glob import glob
+from sklearn.model_selection import GroupKFold, StratifiedKFold
+
+import sklearn
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, log_loss
+from sklearn import metrics
+import warnings
+import pydicom
+
+from torch.utils.data import Dataset,DataLoader
+from torch.utils.data.sampler import SequentialSampler, RandomSampler
+from torch.cuda.amp import autocast, GradScaler
+from torch.nn.modules.loss import _WeightedLoss
+import torch.nn.functional as F
+
+from torch.optim import Adam, SGD
+import torchvision.models as models
+from torch.nn.parameter import Parameter
+from torch.utils.data import DataLoader, Dataset
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts, CosineAnnealingLR, ReduceLROnPlateau
+
+from albumentations import (
+    Compose, OneOf, Normalize, Resize, RandomResizedCrop, RandomCrop, HorizontalFlip, VerticalFlip,
+    RandomBrightness, RandomContrast, RandomBrightnessContrast, Rotate, ShiftScaleRotate, Cutout,
+    IAAAdditiveGaussianNoise, Transpose
+    )
+from albumentations.pytorch import ToTensorV2
+from albumentations import ImageOnlyTransform
+
+# from efficientnet_pytorch import EfficientNet
+
+from scipy.ndimage.interpolation import zoom
+
+# API
+from bottle import post, run, request, response, route
+import pandas as pd
+import joblib
+import json
+
+from datetime import date, datetime
+import time
+
+from os import path
+
+import warnings
+warnings.filterwarnings("ignore")
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
 
 def get_secret():
     secret_name = " "
@@ -46,8 +133,33 @@ def get_secret():
         else:
             return base64.b64decode(get_secret_value_response['SecretBinary'])
 
-..
+# ...
+#
+# ...
 
+def collect_image(id):
+    # collect image
+    url = 'https://../images/{}/{}.jpg?rule=gallery'.format(id[:2], id)
+    image = requests.get(url).content
+
+    if len(image) > 0:
+        nparr = np.fromstring(image, np.uint8)
+        img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        return img_np
+    else:
+        return 0
+    
+    
+@post('/predict/id')
+def predict_id():
+    try:
+        o = json.load(request.body)
+        pred = make_pred(o)
+        return pred
+    except:
+        print('exception')
+
+        
 @route('/health')
 def health_check():
     return 'OK'
